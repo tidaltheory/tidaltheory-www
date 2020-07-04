9dd9cbd686d8672e5099973a338c0a0290ce3713
Jeff <rustyangel@gmail.com>
2020-07-04 16:05:37 -0600

    New: Add Vuex store to load in CMS data

---
+++ store/index.ts
@@ -0,0 +1,33 @@
+import { GetterTree, ActionTree, MutationTree } from 'vuex'
+
+export const state = () => ({
+    pages: [],
+    posts: [],
+  work: [],
+  projects: [],
+  galleries: [],
+})
+
+export type RootState = ReturnType<typeof state>
+
+export const getters: GetterTree<RootState, RootState> = {
+  work: state => state.work,
+}
+
+export const mutations: MutationTree<RootState> = {
+  SET_WORK_POSTS: (state, posts) => (state.work = posts),
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+    async nuxtServerInit({ commit }) {
+        let workData = await require.context('~/assets/content/work/', false, /\.json$/);
+        let workPosts = workData.keys().map(key => {
+          let res = workData(key);
+          res.slug = key.slice(2, -5);
+
+          return res;
+        });
+
+        commit('SET_WORK_POSTS', workPosts);
+      },
+}
