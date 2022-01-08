a106ea5e54188aafc21eb51f4e4aebaf61a44794
Jeff <rustyangel@gmail.com>
2022-01-08 10:34:12 -0700

    New: Add `gallery-card` component

---
+++ components/gallery-card.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        to: {
+            type: String,
+            default: '/',
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="relative flex aspect-[1.3] bg-grey-700">
+        <div class="self-end w-full p-10 bg-grey-900">
+            <NuxtLink class="before:absolute before:inset-0" :to="to">
+                <div
+                    class="
+                        font-display
+                        text-2xl
+                        lg:text-4xl
+                        font-bold
+                        leading-10 leading-trim
+                        uppercase
+                    "
+                >
+                    <slot />
+                </div>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
--- pages/photos/index.vue
+++ pages/photos/index.vue
@@ -1,8 +1,10 @@
 <script>
+import GalleryCard from '~/components/gallery-card.vue'
 import Heading from '~/components/Heading.vue'
 export default {
     components: {
         Heading,
+        GalleryCard,
     },
     async asyncData({ $content, params }) {
         let galleries = await $content('photos', params.slug)
@@ -16,18 +18,17 @@ export default {
 
 <template>
     <div>
-        <Heading>Photos</Heading>
-        <pre>{{ galleries }}</pre>
-        <ul>
-            <li v-for="gallery of galleries" :key="gallery.slug">
-                <NuxtLink :to="`/photos/${gallery.slug}`">
-                    <!-- <img :src="gallery.banner" /> -->
-                    <div>
-                        <h2>{{ gallery.title }}</h2>
-                        <p>{{ gallery.description }}</p>
-                    </div>
-                </NuxtLink>
-            </li>
-        </ul>
+        <div class="pb-[24vh] md:pb-[20vh] xl:pb-[20vh]">
+            <Heading>Photos</Heading>
+        </div>
+        <div class="grid md:grid-cols-2 gap-4 md:gap-12">
+            <GalleryCard
+                v-for="gallery of galleries"
+                :key="gallery.slug"
+                :to="`/photos/${gallery.slug}/`"
+            >
+                {{ gallery.title }}
+            </GalleryCard>
+        </div>
     </div>
 </template>
