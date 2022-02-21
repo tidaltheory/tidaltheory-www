e49e419a1bea23a7b47c520aed29b0d3ba3eb76a
Jeff <rustyangel@gmail.com>
2022-02-21 09:41:43 -0700

    New: Add basic image component

--- content/photos/screen-shots/the-last-of-us.md
+++ content/photos/screen-shots/the-last-of-us.md
@@ -1,4 +1,9 @@
 ---
 title: The Last of Us
 description: Images from The Last of Us
+images: ['tlou_20200127205447']
 ---
+
+Returning to video games after not really playing anything for a few years with this game I’d heard about since it’s initial release on PlayStation 3 was a revelation — in terms of gameplay and story, but also my realisation that “photo mode” was a thing in games now. 
+
+The game’s level of detail, both in environments and in characters, makes for a really rewarding photo mode. I was caught up in landscapes mostly, but started incorporating the characters more as I got used to it.
--- pages/photos/screen-shots/_slug.vue
+++ pages/photos/screen-shots/_slug.vue
@@ -1,14 +1,19 @@
 <script>
+import ImageLens from '~/components/image-lens.vue'
 import PageIntro from '~/components/page-intro.vue'
 import PageSection from '~/components/page-section.vue'
 export default {
     components: {
         PageIntro,
         PageSection,
+        ImageLens,
     },
     async asyncData({ $content, params }) {
         let article = await $content('photos/screen-shots', params.slug).fetch()
-        return { article }
+        let { library } = await $content('imagemeta').fetch()
+        let images = []
+        article.images.forEach((key) => images.push(library[key]))
+        return { article, images }
     },
 }
 </script>
@@ -20,6 +25,13 @@ export default {
         </PageIntro>
         <PageSection>
             <NuxtContent :document="article" />
+            <div>
+                <ImageLens
+                    v-for="(image, i) in images"
+                    :key="i"
+                    :image="image"
+                />
+            </div>
         </PageSection>
     </article>
 </template>
