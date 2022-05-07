2807267b0a941c6c5601a601e9f412eb3a2e316d
Jeff <rustyangel@gmail.com>
2022-05-06 20:28:11 -0600

    New: Add `mention-link` component (#36)

---
+++ components/mention-link.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import Vue from 'vue'
+
+import LogoClari from '~/components/logo-clari.vue'
+import LogoLift from '~/components/logo-lift.vue'
+import LogoShowbie from '~/components/logo-showbie.vue'
+
+const sites = {
+	lift: {
+		href: 'https://liftinteractive.com',
+		logo: LogoLift,
+	},
+	showbie: {
+		href: 'https://www.showbie.com',
+		logo: LogoShowbie,
+	},
+	clari: {
+		href: 'https://www.clari.com',
+		logo: LogoClari,
+	},
+}
+
+export default Vue.extend({
+	props: {
+		site: {
+			type: String,
+			required: true,
+		},
+	},
+
+	computed: {
+		href() {
+			return sites[this.site].href
+		},
+		logo() {
+			return sites[this.site].logo
+		},
+	},
+})
+</script>
+
+<template>
+	<a
+		class="inline-flex items-baseline gap-1 px-1 bg-grey-700 hover:bg-grey-600 text-white rounded align-baseline"
+		:href="href"
+	>
+		<component :is="logo" class="w-[1em] h-[1em] self-center" />
+		<span class="font-semibold tracking-tight">
+			<slot />
+		</span>
+	</a>
+</template>
