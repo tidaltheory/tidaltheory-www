7319b9105ae7fe918e1a059b3fcfb6c36f113a08
Jeff <rustyangel@gmail.com>
2022-08-10 23:05:22 -0600

    Add Projects route

---
+++ src/routes/projects.svelte
@@ -0,0 +1,10 @@
+<script>
+import PageIntro from '../components/page-intro.svelte'
+</script>
+
+<article>
+	<PageIntro>
+		Projects
+		<svelte:fragment slot="intro">Coming soon.</svelte:fragment>
+	</PageIntro>
+</article>
