a1398cfd39af9f69a2507731c61db8863272ee1e
Jeff <rustyangel@gmail.com>
2022-08-15 09:20:03 -0600

    Stagger heading reveal by line

--- src/components/heading.svelte
+++ src/components/heading.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { onMount } from 'svelte'
+
 const resolveHeadingElement = {
 	1: 'h1',
 	2: 'h2',
@@ -17,27 +19,56 @@ export let subtitle = false
 const heading = resolveHeadingElement[level]
 const styleClass = subtitle ? headingStyle.subtitle : headingStyle.title
 
+let hasWrapped = false
 let show: boolean
-$: show = shouldShow !== undefined ? shouldShow : true
+$: show = shouldShow !== undefined && hasWrapped ? shouldShow : true
+
+/**
+ * Wrap each line of the heading in a `span` in order to stagger the reveal
+ * transition styles
+ *
+ * @todo Find a way to wrap each line without relying on `br` tags.
+ */
+function wrapLines(element: HTMLHeadingElement) {
+	let childNodes = element.childNodes
+
+	childNodes.forEach((node, index) => {
+		if (node.nodeType === 3) {
+			let line = document.createElement('span')
+			line.textContent = node.nodeValue
+			line.classList.add('line', 'inline-block', 'will-change-transform')
+			line.setAttribute('style', `--delay: ${50 * index}ms`)
+
+			element.insertBefore(line, node)
+			element.removeChild(node)
+		}
+	})
+
+	hasWrapped = true
+}
+
+let element: HTMLHeadingElement
+onMount(() => wrapLines(element))
 </script>
 
 <svelte:element
 	this={heading}
+	bind:this={element}
 	class="font-bold uppercase font-display leading-none leading-trim {styleClass}"
+	class:line-hidden={!show}
 >
-	<span
-		class="chunk inline-block will-change-transform"
-		class:twist={!show}
-		class:opacity-0={!show}
-	>
-		<slot />
-	</span>
+	<slot />
 </svelte:element>
 
 <style>
-.chunk {
+:global(.line) {
 	transition: opacity 0.7s cubic-bezier(0.165, 0.84, 0.44, 1),
 		transform 0.7s cubic-bezier(0.165, 0.84, 0.44, 1);
 	transition-delay: var(--delay, 0);
+}
+
+.line-hidden :global(.line) {
+	opacity: 0;
+	transform: rotateX(75deg) rotateY(10deg) rotateZ(-9deg);
 }
 </style>
