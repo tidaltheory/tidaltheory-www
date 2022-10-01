31c112a5a32cd94b2ee9c385ac4b681a5bcd0f83
Jeff <rustyangel@gmail.com>
2022-10-01 10:20:12 -0600

    Fix gallery-card hit area

--- src/lib/components/gallery-card.svelte
+++ src/lib/components/gallery-card.svelte
@@ -10,7 +10,7 @@ const thumbnails = cover.thumbnails ?? {}
 // const color = cover ? cover.colors?.[0] : 'rgb(57, 62, 65)'
 </script>
 
-<div class="group relative grid grid-rows-1 bg-grey-700">
+<div class="group relative grid grid-rows-1 rounded-[1px] bg-grey-700">
 	{#if cover}
 		<div class="">
 			<div
@@ -18,7 +18,7 @@ const thumbnails = cover.thumbnails ?? {}
 				style:background-color={cover.colors.dominant}
 			/>
 			<div
-				class="relative grayscale transition-all duration-200 group-hover:grayscale-0"
+				class="relative grayscale transition-all duration-200 group-focus-within:grayscale-0 group-hover:grayscale-0"
 			>
 				<ImageLens
 					image={thumbnails}
@@ -26,24 +26,22 @@ const thumbnails = cover.thumbnails ?? {}
 				/>
 			</div>
 			<div
-				class="absolute inset-0 bg-grey-800 mix-blend-lighten transition-opacity duration-200 group-hover:opacity-0"
+				class="absolute inset-0 bg-grey-800 mix-blend-lighten transition-opacity duration-200"
 			/>
 		</div>
 	{/if}
 	<div class="absolute inset-0 flex">
-		<div
-			class="flex w-full self-end bg-grey-900 bg-opacity-50 p-6 backdrop-blur lg:p-10"
+		<a
+			class="z-10 flex-1 self-end rounded-[1px] before:absolute before:inset-0 {FOCUS_OUTLINE}"
+			href={to}
 		>
-			<a
-				class="z-10 rounded-[1px] before:absolute before:inset-0 {FOCUS_OUTLINE} outline-offset-8"
-				href={to}
-			>
+			<div class="bg-grey-900 bg-opacity-50 p-6 backdrop-blur lg:p-10">
 				<div
 					class="font-bold uppercase tracking-wide font-display text-2xl leading-trim lg:text-4xl"
 				>
 					<slot />
 				</div>
-			</a>
-		</div>
+			</div>
+		</a>
 	</div>
 </div>
