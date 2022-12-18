c4ac6b62eabb1c8ca671519ba82fb739f0714f7f
Jeff <rustyangel@gmail.com>
2022-12-18 09:38:43 -0700

    Add placeholder Writing page

---
+++ src/routes/writing/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+import PageIntro from '$lib/components/page-intro.svelte'
+import PageSection from '$lib/components/page-section.svelte'
+</script>
+
+<svelte:head>
+	<title>Writing — Tidal Theory</title>
+</svelte:head>
+
+<article>
+	<PageIntro>
+		Writing
+		<svelte:fragment slot="intro">Coming soon.</svelte:fragment>
+	</PageIntro>
+	<PageSection>
+		<div class="prose prose-invert">
+			<a href="/writing/difference-single-key-light-can-make/"
+				>The Difference a Single Key Light Can Make</a
+			>
+		</div>
+	</PageSection>
+</article>
