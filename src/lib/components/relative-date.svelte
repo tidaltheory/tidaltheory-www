1f2bb20adc98037d99d04c6648ec33b1a25c835d
Jeff <rustyangel@gmail.com>
2023-02-06 12:11:16 -0700

    Remove time from recent dates

--- src/lib/components/relative-date.svelte
+++ src/lib/components/relative-date.svelte
@@ -15,6 +15,9 @@ let relativeDate =
 	differenceInCalendarDays(now, datestamp) > 6
 		? formatDistanceToNow(datestamp, { addSuffix: true })
 		: formatRelative(datestamp, now, { weekStartsOn: 1 })
+				// Remove time text from date string.
+				.replace(/at.*$/, '')
+				.trimEnd()
 </script>
 
 <time class="text-grey-400 text-xs" datetime={formatISO(datestamp)}
