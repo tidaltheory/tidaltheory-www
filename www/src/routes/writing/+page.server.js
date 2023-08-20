1d790141672ee437cbfe9db84ab160095a2f885c
Jeff <rustyangel@gmail.com>
2023-08-19 22:49:47 -0600

    Update Writing index layout

--- www/src/routes/writing/+page.server.js
+++ www/src/routes/writing/+page.server.js
@@ -1,12 +1,27 @@
 import { error } from '@sveltejs/kit'
 
+import { getYear } from 'date-fns'
+
 import { getPosts } from '$lib/sanity/posts.js'
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async () => {
 	const posts = await getPosts()
 
-	if (posts) return { posts }
+	if (posts) {
+		const postsByYear = new Map()
+
+		for (const post of posts) {
+			const date = new Date(post._createdAt)
+			const year = getYear(date)
+
+			if (!postsByYear.has(year)) postsByYear.set(year, new Set())
+
+			postsByYear.get(year).add(post)
+		}
+
+		return { posts: postsByYear }
+	}
 
 	throw error(404, 'Not found.')
 }
--- www/src/routes/writing/+page.svelte
+++ www/src/routes/writing/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import Divider from '$lib/components/divider.svelte'
 import PageIntro from '$lib/components/page-intro.svelte'
 import PageSection from '$lib/components/page-section.svelte'
 
@@ -14,14 +15,28 @@ $: ({ posts } = data)
 </svelte:head>
 
 <article>
-	<PageIntro>
-		Writing
-		<!-- <TextLede slot="intro">Coming soon.</TextLede> -->
-	</PageIntro>
+	<PageIntro>Writing</PageIntro>
 	<PageSection>
-		<div class="prose prose-invert">
-			{#each posts as post}
-				<a href="/writing/{post.slug.current}/">{post.title}</a>
+		<div class="prose prose-invert flex flex-col gap-3">
+			{#each posts.keys() as set}
+				<Divider />
+				<div class="flex">
+					<div class="w-3/12">
+						<span class="block py-1">
+							{set}
+						</span>
+					</div>
+					<div class="flex flex-col gap-3 w-full">
+						{#each posts.get(set) as post, index}
+							{#if index !== 0}<Divider />{/if}
+							<a
+								class="block py-1"
+								href="/writing/{post.slug.current}/"
+								>{post.title}</a
+							>
+						{/each}
+					</div>
+				</div>
 			{/each}
 		</div>
 	</PageSection>
