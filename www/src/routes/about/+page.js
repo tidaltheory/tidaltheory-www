d4febccf745c080a13d05c10345603a54ccab55d
Jeff <rustyangel@gmail.com>
2023-11-11 13:58:59 -0700

    Update page load

--- www/src/routes/about/+page.js
+++ www/src/routes/about/+page.js
@@ -1,9 +1,11 @@
 /** @type {import('./$types').PageLoad} */
 export const load = async ({ fetch }) => {
-	let films = new Response([])
+	let films = []
 
 	try {
-		films = await fetch('/.netlify/functions/letterboxd')
+		films = await fetch('/.netlify/functions/letterboxd').then((response) =>
+			response.json(),
+		)
 	} catch {}
 	// Const films = await fetch('/.netlify/functions/letterboxd').then(
 	// 	(response) => response.json(),
@@ -11,7 +13,7 @@ export const load = async ({ fetch }) => {
 
 	return {
 		streamed: {
-			films: films.json(),
+			films,
 		},
 	}
 }
