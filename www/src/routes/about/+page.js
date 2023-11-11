afdd611a5e32a6a19b7db6c58e3d97bfe4fa1c1d
Jeff <rustyangel@gmail.com>
2023-11-11 14:34:22 -0700

    Log error in page load

--- www/src/routes/about/+page.js
+++ www/src/routes/about/+page.js
@@ -6,10 +6,9 @@ export const load = async ({ fetch }) => {
 		films = await fetch('/.netlify/functions/letterboxd').then((response) =>
 			response.json(),
 		)
-	} catch {}
-	// Const films = await fetch('/.netlify/functions/letterboxd').then(
-	// 	(response) => response.json(),
-	// )
+	} catch (error) {
+		console.log('PageLoad error:', error)
+	}
 
 	return {
 		streamed: {
