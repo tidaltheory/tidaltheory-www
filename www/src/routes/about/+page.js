3977fc1827aca13667bbe14636abac24a804ace9
Jeff <rustyangel@gmail.com>
2023-11-12 14:24:14 -0700

    Try fetching full URL

--- www/src/routes/about/+page.js
+++ www/src/routes/about/+page.js
@@ -3,16 +3,18 @@ export const load = async ({ fetch }) => {
 	let films = []
 
 	try {
-		films = await fetch('/.netlify/functions/letterboxd').then((response) =>
-			response.json(),
-		)
+		films = await fetch(
+			'https://deploy-preview-150--tidaltheory.netlify.app/.netlify/functions/letterboxd',
+		).then((response) => response.json())
 	} catch (error) {
 		console.log('PageLoad error:', error)
 	}
 
 	return {
 		streamed: {
-			films,
+			films: new Promise((resolve) => {
+				resolve(films)
+			}),
 		},
 	}
 }
