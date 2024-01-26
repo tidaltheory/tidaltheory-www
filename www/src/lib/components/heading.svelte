ba75588eb781ca611230d799874b17b0598d1c11
Jeff <rustyangel@gmail.com>
2024-01-25 21:25:35 -0700

    Balance heading text

--- www/src/lib/components/heading.svelte
+++ www/src/lib/components/heading.svelte
@@ -53,7 +53,7 @@ onMount(() => {
 
 <svelte:element
 	this={heading}
-	class="font-display leading-trim font-bold uppercase leading-none {styleClass} m-0 text-white empty:hidden"
+	class="font-display leading-trim text-balance font-bold uppercase leading-none {styleClass} m-0 text-white empty:hidden"
 	class:line-hidden={!show}
 >
 	<span class="line inline-block will-change-transform" bind:this={element}>
