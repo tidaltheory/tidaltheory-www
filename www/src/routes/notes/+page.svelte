dc2472c257f50ab376d2ff1c29e1f819ce72af8e
Jeff <rustyangel@gmail.com>
2025-01-04 19:28:40 -0700

    Fix links in Notes and Writing pages

--- www/src/routes/notes/+page.svelte
+++ www/src/routes/notes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import { FOCUS_OUTLINE } from '$lib/classnames.js'
 import Divider from '$lib/components/divider.svelte'
 import PageIntro from '$lib/components/page-intro.svelte'
 import PageSection from '$lib/components/page-section.svelte'
@@ -25,11 +26,16 @@ $: ({ notes } = data)
 		<div class="flex flex-col gap-8">
 			{#each notes as note, index}
 				{#if index !== 0}<Divider />{/if}
-				<div class="prose prose-invert prose-lg md:prose-xl">
+				<div class="prose prose-lg md:prose-xl">
 					<PortableText value={note.content} />
-					<a href="/notes/{note.slug.current}/">
-						<RelativeDate date={note._createdAt} />
-					</a>
+					<span class="not-prose text-grey-400">
+						<a
+							class="inline-flex rounded-[1px] underline decoration-[transparent] decoration-[0.0781em] underline-offset-[calc(0.0781em_*_2)] transition hover:decoration-[inherit] {FOCUS_OUTLINE}"
+							href="/notes/{note.slug.current}/"
+						>
+							<RelativeDate date={note._createdAt} />
+						</a>
+					</span>
 				</div>
 			{/each}
 		</div>
--- www/src/routes/writing/+page.svelte
+++ www/src/routes/writing/+page.svelte
@@ -17,20 +17,20 @@ $: ({ posts } = data)
 <article>
 	<PageIntro>Writing</PageIntro>
 	<PageSection>
-		<div class="prose prose-invert flex flex-col gap-3">
+		<div class="flex flex-col gap-3">
 			{#each posts.keys() as set}
 				<Divider />
 				<div class="flex">
-					<div class="w-3/12">
+					<div class="year-column">
 						<span class="block py-1">
 							{set}
 						</span>
 					</div>
-					<div class="flex flex-col gap-3 w-full">
+					<div class="flex w-full flex-col gap-3">
 						{#each posts.get(set) as post, index}
 							{#if index !== 0}<Divider />{/if}
 							<a
-								class="block py-1"
+								class="post-link block py-1"
 								href="/writing/{post.slug.current}/"
 								>{post.title}</a
 							>
@@ -41,3 +41,27 @@ $: ({ posts } = data)
 		</div>
 	</PageSection>
 </article>
+
+<style>
+.year-column {
+	inline-size: 25%;
+
+	@media (min-width: 768px) {
+		inline-size: 15%;
+	}
+
+	@media (min-width: 1280px) {
+		inline-size: 14%;
+	}
+}
+
+.post-link {
+	color: #fff;
+	font-weight: 500;
+	text-decoration-line: underline;
+	text-decoration-thickness: 0.0781em;
+	text-underline-offset: calc(0.0781em * 2);
+	transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+	transition-property: text-decoration-color, outline-width;
+}
+</style>
