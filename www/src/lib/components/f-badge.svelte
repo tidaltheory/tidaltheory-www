595a96fe325fbe36b23688ae102ac10783939511
Jeff <rustyangel@gmail.com>
2025-02-02 15:11:51 -0700

    Prevent text wrapping in Badge component

--- www/src/lib/components/f-badge.svelte
+++ www/src/lib/components/f-badge.svelte
@@ -2,7 +2,7 @@
 	class="bg-grey-700 flex rounded px-[0.375rem] py-1 lg:px-2 lg:py-[0.375rem]"
 >
 	<span
-		class="leading-trim salt text-grey-200 block text-xs uppercase lg:text-sm"
+		class="leading-trim salt text-grey-200 block text-nowrap text-xs uppercase lg:text-sm"
 	>
 		<slot />
 	</span>
