d47cf431bd9fd40d4bc3b7b01dc9f6eab75f9cc1
Jeff <rustyangel@gmail.com>
2025-02-05 10:19:24 -0700

    Fix transitioning elements stacking above bg

--- www/src/lib/components/helpers/fade-up.svelte
+++ www/src/lib/components/helpers/fade-up.svelte
@@ -5,7 +5,7 @@ export let className = undefined
 </script>
 
 <div
-	class="fade-up {className}"
+	class="fade-up {className ?? ''}"
 	class:translate-y-[1.5vh]={!showing}
 	class:opacity-0={!showing}
 	style="--delay: {delay}ms"
@@ -18,6 +18,6 @@ export let className = undefined
 	transition:
 		transform 0.6s cubic-bezier(0.645, 0.045, 0.355, 1),
 		opacity 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-	transition-delay: var(--delay, 0);
+	transition-delay: var(--delay, 0ms);
 }
 </style>
--- www/src/routes/+layout.svelte
+++ www/src/routes/+layout.svelte
@@ -13,7 +13,7 @@ let bg
 <div class="relative grid min-h-[100svh]">
 	<ObserveIntersection once element={bg} let:intersecting>
 		<div
-			class="bg pointer-events-none absolute inset-0"
+			class="bg pointer-events-none absolute inset-0 z-10"
 			class:opacity-0={!intersecting}
 			bind:this={bg}
 		/>
