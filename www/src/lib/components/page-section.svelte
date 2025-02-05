12ec84bd6e352f69a5e8175ac8cda97901e7d52f
Jeff <rustyangel@gmail.com>
2025-02-05 13:04:17 -0700

    Fix container-based padding

--- www/src/lib/components/page-section.svelte
+++ www/src/lib/components/page-section.svelte
@@ -7,27 +7,34 @@ export let fullscreen = false
 <!--	class:min-h-[calc(100svh_-_4rem)]={fullscreen}-->
 <!--&gt;-->
 <div class="section" class:full-height={fullscreen}>
-	<slot />
+	<div class="section-padding">
+		<slot />
+	</div>
 </div>
 
 <style>
 .section {
 	display: grid;
 	max-inline-size: 1440px;
-	container: inline-size;
-	padding: 9svb 32px 18.5svb;
+	container: section / inline-size;
 	margin-inline: auto;
-
-	@media (min-width: 768px) {
-		padding: 11svb 10cqi 22.5svb;
-	}
-
-	@media (min-width: 1280px) {
-		padding: 11svb 11cqi 22svb;
-	}
 }
 
 .full-height {
 	min-block-size: calc(100svb - 64px);
+}
+
+.section-padding {
+	padding: 9svb 32px 18.5svb;
+
+	@container section (width) {
+		@media (min-width: 768px) {
+			padding: 11svb 10cqi 22.5svb;
+		}
+
+		@media (min-width: 1280px) {
+			padding: 11svb 11cqi 22svb;
+		}
+	}
 }
 </style>
