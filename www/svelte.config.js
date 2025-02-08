702086010b2bfef875788a7547db7c8721f6caa7
Jeff <rustyangel@gmail.com>
2025-02-08 10:48:41 -0700

    Remove mdsvex

--- www/svelte.config.js
+++ www/svelte.config.js
@@ -1,20 +1,13 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
-import { mdsvex } from 'mdsvex'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({ postcss: true }),
-		mdsvex({
-			extensions: ['.md'],
-		}),
-	],
+	preprocess: [preprocess({ postcss: true })],
 
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte'],
 
 	kit: {
 		adapter: adapter({
