86c116a6bb8c95e1d9c9454ff0cf07253c77d263
Jeff <rustyangel@gmail.com>
2025-04-17 13:59:12 -0600

    Update debounce types

--- www/src/lib/debounce.js
+++ www/src/lib/debounce.js
@@ -1,8 +1,10 @@
 /**
- * @param {Function} function_
- * @param {number} delay
- * @param {{ leading?: boolean }} [options]
- * @returns {Function}
+ * @template {(...args: unknown[]) => unknown} T
+ * @param {T} function_ - The function to debounce
+ * @param {number} [delay=0] - The number of milliseconds to delay
+ * @param {Object} [options={}] - Additional options for debouncing
+ * @param {boolean} [options.leading=false] - Specify invoking on the leading edge of the timeout
+ * @returns {(...args: Parameters<T>) => ReturnType<T>} A debounced version of the original function
  */
 export function debounce(function_, delay, { leading } = {}) {
 	let timerId
@@ -17,7 +19,6 @@ export function debounce(function_, delay, { leading } = {}) {
 		}
 
 		clearTimeout(timerId)
-
 		timerId = setTimeout(() => shouldInvoke && function_(...args), delay)
 	}
 }
